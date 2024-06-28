cf557d8d08576734a4b1ea21cb05b101ec88bcd3
Mercy <durumercy206@gmail.com>
2024-06-28 09:41:10 +0100

    Mobile Header Update

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
         style={{ backgroundImage: `url(${coverPhoto})` }}
         className="bg-cover bg-center md:bg-fixed max-w-[100%] w-full h-[100dvh] md:h-[500px] relative"
       >
-        <img src={logo} alt="" className="w-[250px] absolute z-[100000]" />
-        <div className="flex flex-col justify-center items-center text-white relative z-[100000] h-[650px] mx-auto max-w-[70%] md:max-w-[50%] ">
+       <div className="flex justify-center md:block"> <img src={logo} alt="" className="w-[250px] absolute z-[100000]" /></div>
+        <div className="flex flex-col justify-center items-center text-white relative z-[100000] h-[700px] mx-auto max-w-[70%] md:max-w-[50%] ">
           <h2 className="text-3xl lg:text-4xl font-bold md:leading-10 md:text-center">
             Welcome to the Africa CDC Systems and Dashboards Access Page
           </h2>
@@ -19,7 +19,7 @@ const Header = () => {
           </p>
         </div>
       </header>
-      <div className="absolute top-0 left-0 bg-black w-full h-[100dvh] md:h-[500px] z-[10000] !opacity-30"></div>
+      <div className="absolute top-0 left-0 bg-black w-full h-[100dvh] md:h-[500px] z-[10000] !opacity-40 md:!opacity-30"></div>
     </>
   );
 };
