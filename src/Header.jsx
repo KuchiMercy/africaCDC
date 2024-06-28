349f0037ff27952d6ec3a6436fc07c0c761d66be
Mercy <durumercy206@gmail.com>
2024-06-28 09:33:29 +0100

    Mobile text update

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,10 +10,10 @@ const Header = () => {
       >
         <img src={logo} alt="" className="w-[250px] absolute z-[100000]" />
         <div className="flex flex-col justify-center items-center text-white relative z-[100000] h-[650px] mx-auto max-w-[70%] md:max-w-[50%] ">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold md:leading-10 md:text-center">
+          <h2 className="text-3xl lg:text-4xl font-bold md:leading-10 md:text-center">
             Welcome to the Africa CDC Systems and Dashboards Access Page
           </h2>
-          <p className="text-md md:text-lg my-4 md:text-center">
+          <p className="text-lg my-4 md:text-center">
             A one stop center for finding systems and dashboards within Africa
             CDC
           </p>
