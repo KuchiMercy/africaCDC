d51c16b70bf218cbc26a0ba672f72e1d5649c4fd
Mercy <durumercy206@gmail.com>
2024-06-28 09:23:22 +0100

    set the height of image for mobile view

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <>
       <header
         style={{ backgroundImage: `url(${coverPhoto})` }}
-        className="bg-cover bg-center md:bg-fixed max-w-[100%] w-full h-full md:h-[500px] relative"
+        className="bg-cover bg-center md:bg-fixed max-w-[100%] w-full h-[100dvh] md:h-[500px] relative"
       >
         <img src={logo} alt="" className="w-[250px] absolute z-[100000]" />
         <div className="flex flex-col justify-center items-center text-white relative z-[100000] h-[600px] mx-auto max-w-[70%] md:max-w-[50%] ">
@@ -19,7 +19,7 @@ const Header = () => {
           </p>
         </div>
       </header>
-      <div className="absolute top-0 left-0 bg-black w-full h-[600px] md:h-[500px] z-[10000] !opacity-30"></div>
+      <div className="absolute top-0 left-0 bg-black w-full h-[100dvh] md:h-[500px] z-[10000] !opacity-30"></div>
     </>
   );
 };
